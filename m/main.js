e8f7b2b9d2c4ec7b3f47eba3337744653e9eefd7
kai1.xu <kai1.xu@zhangmen.com>
2018-09-09 03:21:58 +0800

    终于完成了

---
+++ m/main.js
@@ -0,0 +1,42 @@
+;(function(){
+  let getEles = document.querySelectorAll.bind(document),
+      getElem = document.querySelector.bind(document),
+      _menuLen = getEles(".m_main_menu li"),
+      _conLen = getEles(".tabCon")
+  let index = this.index || 0 ;
+
+ let _playEvent = {
+
+    tabEvent: function(){
+      let _self = this;
+      [].forEach.call(_menuLen, (val, index) => {
+          val.addEventListener("click", e =>{
+             val.classList.add("curr")
+              _self.removeCon();
+              _self.removeCurrent();
+              _conLen[index].style.display = "block";
+          });
+      });
+    },
+    removeCurrent:function(){
+      let _self = this;
+      [].forEach.call(_menuLen,(val,index)=>{
+          val.classList.remove("curr");
+      })
+    },
+    removeCon: function() {
+        [].forEach.call(_conLen, function(val, index) {
+            //console.log(val)
+            val.style.display = "none";
+        });
+    },
+    init:function(){
+      this.tabEvent(),
+      this.removeCurrent(),
+      this.removeCon(),
+      _menuLen[0].click();
+    }
+ }
+ _playEvent.init()
+
+})()
